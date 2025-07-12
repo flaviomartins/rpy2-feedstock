dd614cfe01b90f4113fc4385d31097bf03db4b37
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-12 01:46:18 +0100

    Add build script and update meta.yaml for rpy2 package

---
+++ recipe/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib/R/lib"
+
+${PYTHON} -m pip install --no-deps --no-build-isolation . -vv
