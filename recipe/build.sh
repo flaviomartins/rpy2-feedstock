dad0231cee6908d6ea20c6501c5c9a46ee7df0fe
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-12 01:53:07 +0100

    Remove unnecessary export of CFLAGS and LDFLAGS in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,4 @@
 
 set -euxo pipefail
 
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib/R/lib"
-
 ${PYTHON} -m pip install --no-deps --no-build-isolation . -vv
