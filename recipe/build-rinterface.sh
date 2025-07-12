8c914485f71f2ddac5e2347432bbf1fac8a2d55e
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-12 13:02:44 +0100

    Remove LDFLAGS export from build scripts

--- recipe/build-rinterface.sh
+++ recipe/build-rinterface.sh
@@ -5,6 +5,5 @@ set -euxo pipefail
 cd rpy2-rinterface
 
 export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib/R/lib"
 
 ${PYTHON} -m pip install --no-deps --no-build-isolation . -vv
