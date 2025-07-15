9a327dc74d3b14ada8acb704d4cb7d8a287592af
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-15 23:06:56 +0100

    Update build-rinterface.sh

--- recipe/build-rinterface.sh
+++ recipe/build-rinterface.sh
@@ -6,8 +6,8 @@ cd rpy2-rinterface
 
 if [[ "${build_platform}" != "${target_platform}" ]]; then
   echo "Cross-compiling"
-  export LDFLAGS="-L${BUILD_PREFIX}/lib/R/lib ${LDFLAGS}"
-  export CPPFLAGS="-I${BUILD_PREFIX}/lib/R/include ${CPPFLAGS}"
+  export LDFLAGS="-L${PREFIX}/lib/R/lib ${LDFLAGS}"
+  export CPPFLAGS="-I${PREFIX}/lib/R/include ${CPPFLAGS}"
 fi
 
 ${PYTHON} -m pip install . -vv
