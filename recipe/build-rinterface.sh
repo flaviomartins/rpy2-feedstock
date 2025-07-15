6a6360538f98ec15a7b72f2d5abcc158a6a83fd7
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-15 23:23:30 +0100

    Update build-rinterface.sh

--- recipe/build-rinterface.sh
+++ recipe/build-rinterface.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+# Prevent running ldconfig when cross-compiling.
+if [[ "${BUILD}" != "${HOST}" ]]; then
+  echo "#!/usr/bin/env bash" > ldconfig
+  chmod +x ldconfig
+  export PATH=${PWD}:$PATH
+fi
+
 cd rpy2-rinterface
 
 if [[ "${build_platform}" != "${target_platform}" ]]; then
