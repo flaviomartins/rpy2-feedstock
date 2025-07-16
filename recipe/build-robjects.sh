364c7988ed6159e0bed017b22e7d4ebb1c9d2202
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-16 11:56:46 +0100

    Fix: Set RPY2_CFFI_MODE to "API" for cross-compilation in build scripts

--- recipe/build-robjects.sh
+++ recipe/build-robjects.sh
@@ -2,16 +2,13 @@
 
 set -euxo pipefail
 
-if [[ "${target_platform}" == "osx-64" || "${target_platform}" == "linux-aarch64" || "${target_platform}" == "linux-ppc64le" ]]; then
-  export RPY2_CFFI_MODE="API"
-fi
-
 cd rpy2-robjects
 
 if [[ "${build_platform}" != "${target_platform}" ]]; then
-  echo "Cross-compiling"
-  export LDFLAGS="-L${PREFIX}/lib/R/lib ${LDFLAGS}"
-  export CPPFLAGS="-I${PREFIX}/lib/R/include ${CPPFLAGS}"
+  export RPY2_CFFI_MODE="API"
 fi
+
+export LDFLAGS="-L${PREFIX}/lib/R/lib ${LDFLAGS}"
+export CPPFLAGS="-I${PREFIX}/lib/R/include ${CPPFLAGS}"
 
 ${PYTHON} -m pip install . -vv
