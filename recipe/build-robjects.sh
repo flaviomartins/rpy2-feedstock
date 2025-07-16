0c985fe7381632666c73b3024269c5c53f401bc4
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-16 19:19:50 +0100

    Fix: Update build scripts to remove unnecessary directory changes and streamline installation commands

--- recipe/build-robjects.sh
+++ recipe/build-robjects.sh
@@ -2,8 +2,6 @@
 
 set -euxo pipefail
 
-cd rpy2-robjects
-
 if [[ "${build_platform}" != "${target_platform}" ]]; then
   export RPY2_CFFI_MODE="API"
 fi
@@ -11,7 +9,4 @@ fi
 export LDFLAGS="-L${PREFIX}/lib/R/lib ${LDFLAGS}"
 export CPPFLAGS="-I${PREFIX}/lib/R/include ${CPPFLAGS}"
 
-${PYTHON} -m pip install . -vv
-
-mkdir -p $SP_DIR/rpy2
-echo "__path__ = __import__('pkgutil').extend_path(__path__, __name__)" > $SP_DIR/rpy2/__init__.py
+${PYTHON} -m pip install ./rpy2-robjects/ . -vv
