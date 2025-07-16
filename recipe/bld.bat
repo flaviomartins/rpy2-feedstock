63b9f5f245a1618be40e88cdcca4a2b37f816917
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-16 21:38:21 +0100

    Fix: Update setup.cfg path in bld.bat to use rpy2-rinterface directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 @echo on
 
-echo [build] > setup.cfg
-echo compiler = mingw32 >> setup.cfg
+echo [build] > rpy2-rinterface/setup.cfg
+echo compiler = mingw32 >> rpy2-rinterface/setup.cfg
 set RPY2_API_FORCE=True
 
 %PYTHON% -m pip install ./rpy2-rinterface/ ./rpy2-robjects/ . -vv -C--global-option=--verbose
